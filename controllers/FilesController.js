3c3c5f30b205f50f60d8f72723b28ef320bd1656
badrxd <badrxdxd@gmail.com>
2024-03-09 08:28:22 +0100

    reje3t l7za9 lblasto

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -193,59 +193,44 @@ class FilesController {
       }
       const userInDb = await mongodbClient.db
         .collection('users')
-        .findOne({ _id: ObjectID(idUser) });
+        .findOne({ _id: ObjectID(idUser.toString()) });
 
       if (!userInDb) {
         return res.status(401).send({ error: 'Unauthorized' });
       }
 
-      const fileId = await req.params.id;
+      const { id: fileId } = await req.params;
 
-      // const file = await mongodbClient.db
-      //   .collection('files')
-      //   .findOne({ _id: ObjectID(fileId) });
+      const file = await mongodbClient.db
+        .collection('files')
+        .findOne({ _id: ObjectID(fileId.toString()) });
 
-      // if (!file || file.userId !== idUser) {
-      //   return res.status(404).send({ error: 'Not found' });
-      // }
-
-      const newValue = { $set: { isPublic: true } };
-      const options = { returnOriginal: 'after' };
-      const updatedFile = await mongodbClient.db
-        .collection('files').findOneAndUpdate(
-          { _id: ObjectID(fileId), userId: userInDb.id },
-          newValue,
-          options,
-        );
-      console.log(updatedFile);
-      if (!updatedFile.value) {
-        return res.status(404).json({ error: 'File not found' });
+      if (!file || file.userId !== idUser) {
+        return res.status(404).send({ error: 'Not found' });
       }
+      const {
+        _id: id,
+        userId,
+        name,
+        type,
+        parentId,
+      } = file;
 
-      // const {
-      //   _id,
-      //   userId,
-      //   name,
-      //   type,
-      //   parentId,
-      // } = file;
+      await mongodbClient.db
+        .collection('files')
+        .updateOne(
+          { _id: ObjectID(fileId.toString()) },
+          { $set: { isPublic: true } },
+        );
 
-      // await mongodbClient.db
-      //   .collection('files')
-      //   .updateOne(
-      //     { _id: ObjectID(fileId) },
-      //     { $set: { isPublic: true } },
-      //   );
-
-      // return res.status(200).send({
-      //   id: _id,
-      //   userId,
-      //   name,
-      //   type,
-      //   isPublic: true,
-      //   parentId,
-      // });
-      return res.status(200).json(updatedFile.value);
+      return res.status(200).send({
+        id,
+        userId,
+        name,
+        type,
+        isPublic: true,
+        parentId,
+      });
     } catch (error) {
       return res.status(404).send({ error: 'Not found' });
     }
