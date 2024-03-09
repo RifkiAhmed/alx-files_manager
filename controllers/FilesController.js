5b812599e682940d4d9c1f90cf99087ca53000e5
ahmed <rifkiahmed.91@gmail.com>
2024-03-09 12:26:17 +0100

    Handle error when  file id is not a valid ObjectId

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -206,7 +206,7 @@ class FilesController {
         parentId,
       });
     } catch (error) {
-      return res.status(500).send('');
+      return res.status(404).send({ error: 'Not found' });
     }
   }
 
@@ -250,7 +250,7 @@ class FilesController {
         parentId,
       });
     } catch (error) {
-      return res.status(500).send('');
+      return res.status(404).send({ error: 'Not found' });
     }
   }
 
